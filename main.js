b9693f85d92978e888d86c7e36eea1f8cb734a32
Vitor Abreu <vitorvaf@gmail.com>
2020-11-22 18:05:06 -0300

    Criando menu personalizado

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-const {app, BrowserWindow}  = require('electron');
+const { app, BrowserWindow, Menu } = require("electron");
 
 var mainWindow = null;
 
-const createWindow = async() =>{
-    mainWindow = new BrowserWindow({
-        width:800,
-        height:600
-    });
+const createWindow = async () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
 
-    await mainWindow.loadFile('src/pages/editor/index.html');
-}
+  await mainWindow.loadFile("src/pages/editor/index.html");
+};
 
+/// TEAMPLATE MENU
+
+const teamplateMenu =  [
+    {
+        
+    }
+]
 
 app.whenReady().then(createWindow);
+
+/// ACTIVATE
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
